39e9d31c66465c7019756c572b9447dca4fa2d8c
_NSAKEY <root [at] abigisp [dot] com>
2015-02-02 14:29:31 -0600

    Bolted sudo on and made the script more verbose.

--- happy-dance.sh
+++ happy-dance.sh
@@ -2,29 +2,22 @@
 
 ###
 # happy-dance.sh by _NSAKEY
-# Requirements: OpenSSH 6.5 or above
+# Requirements: OpenSSH 6.5 or above, sudo access.
 # This script just automates the steps laid out in stribika's Secure Secure Shell guide.
 # Source: https://stribika.github.io/2015/01/04/secure-secure-shell.html
 ###
 
 PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "This script will give you an ssh config for clients and servers that should force the NSA to work for a living.
-Use -h for help
+echo "This script will give you an ssh config for clients and servers that should force the NSA to work for a living. 
+Use -h for help.
 "
 
-# root is required in order to copy the config files to /etc/ssh.
-
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root" 1>&2
-    exit 1
-fi
-
-# The ssh_client function takes the time to check for the existence of keys because deleting or overwriting existing keys would be bad. 
-# This will generate keys for the root user. You will want to move and chown the generated public/private keypairs to suit your environment.
+# The ssh_client function takes the time to check for the existence of keys because deleting or overwriting existing keys would be bad.
 
 ssh_client() {
-    cp $PWD/etc/ssh/ssh_config /etc/ssh/ssh_config
+    echo "Replacing your ssh client configuration file..."
+    sudo cp $PWD/etc/ssh/ssh_config /etc/ssh/ssh_config
 
     if [ ! -f $HOME/.ssh/id_ed25519 ]; then
         ssh-keygen -t ed25519 -o -a 100
@@ -45,23 +38,26 @@ ssh_client() {
 ssh_server() {
     read -p "This option destroys all host keys. Are you sure want to proceed? (y/n)"
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        cp $PWD/etc/ssh/sshd_config /etc/ssh/sshd_config
+        echo "Replacing your ssh server configuration file..."
+        sudo cp $PWD/etc/ssh/sshd_config /etc/ssh/sshd_config
 
         if [ ! -f /etc/ssh/moduli ]; then
             echo "Your OS doesn't have an /etc/ssh/moduli file, so we have to generate one. This might take a while."
-            ssh-keygen -G "${HOME}/moduli" -b 4096
-            ssh-keygen -T /etc/ssh/moduli -f "${HOME}/moduli"
-            rm "${HOME}/moduli"
+            sudo ssh-keygen -G "${HOME}/moduli" -b 4096
+            sudo ssh-keygen -T /etc/ssh/moduli -f "${HOME}/moduli"
+            sudo rm "${HOME}/moduli"
         else
-            awk '$5 > 2000' /etc/ssh/moduli > "${HOME}/moduli"
-            mv "${HOME}/moduli" /etc/ssh/moduli
+            echo "Modifying your /etc/ssh/module"
+            sudo awk '$5 > 2000' /etc/ssh/moduli > "${HOME}/moduli"
+            sudo mv "${HOME}/moduli" /etc/ssh/moduli
         fi
 
         cd /etc/ssh
-        rm ssh_host_*key*
-        ssh-keygen -t ed25519 -f ssh_host_ed25519_key -q -N "" < /dev/null
-        ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -q -N "" < /dev/null
-        /etc/init.d/ssh restart
+        sudo rm ssh_host_*key*
+        sudo ssh-keygen -t ed25519 -f ssh_host_ed25519_key -q -N "" < /dev/null
+        sudo ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -q -N "" < /dev/null
+        sudo /etc/init.d/ssh restart
+        echo "Without closing this ssh session, add your public key to ~/.ssh/authorized_keys if it isn't there already and try logging in. If it works, HAPPY DANCE!"
     else
         exit;
     fi
@@ -77,15 +73,17 @@ while getopts "hcs" opt; do
             -s  Set up a server
             "
         ;;
+
         c)
             ssh_client
         ;;
+
         s)
             ssh_server
         ;;
+
         \?)
             echo "$opt is invalid."
         ;;
     esac
 done
-
