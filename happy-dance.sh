5dce335d142538ff20d5d1a3162c9adea1e4bfa7
_NSAKEY <root [at] abigisp [dot] com>
2015-05-30 19:10:23 -0500

    Added more comments and did some clean-up.

--- happy-dance.sh
+++ happy-dance.sh
@@ -3,7 +3,10 @@
 ###
 # happy-dance.sh by _NSAKEY
 # Requirements: OpenSSH 6.5 or above, sudo access.
-# This script just automates the steps laid out in stribika's Secure Secure Shell guide.
+# Tested on Debian Wheezy/Jessie (With ssh from wheezy-backports for Wheezy),
+# Ubuntu 14.04/15.04, CentOS 7, FreeBSD 10, and Solaris 11.2 with CSWOpenSSH.
+# Note for Solaris users: Change the shell to /bin/bash and It Just Works.
+# This script automates everything laid out in stribika's Secure Secure Shell.
 # Source: https://stribika.github.io/2015/01/04/secure-secure-shell.html
 ###
 
@@ -11,9 +14,12 @@ PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "This script will give you an ssh config for clients and servers that should force the NSA to work for a living.
 Use -h for help.
+For an explanation of everything, check out Secure Secure Shell:
+https://stribika.github.io/2015/01/04/secure-secure-shell.html
 "
 
-# The ssh_client function takes the time to check for the existence of keys because deleting or overwriting existing keys would be bad.
+# The ssh_client function takes the time to check for the existence of keys
+# because deleting or overwriting existing keys would be bad.
 
 ssh_client() {
     echo "Replacing your ssh client configuration file..."
@@ -32,8 +38,12 @@ ssh_client() {
     fi
 }
 
-# Meanwhile, the ssh_server function asks if you're sure you want to obliterate the public/private keypairs which make up the host keys.
-# After that, /etc/ssh/moduli is either hardened or generated in a hardened state and then the ED25519 and 4096-bit RSA host keys are generated. As having passwords on host keys means that sshd won't start automatically, the choice of passwording them has been removed from the user.
+# Meanwhile, the ssh_server function asks if you're sure you want to
+# obliterate the public/private keypairs which make up the host keys.
+# After that, /etc/ssh/moduli is either hardened or generated in a hardened
+# state and then the ED25519 and 4096-bit RSA host keys are generated. As
+# having passwords on host keys means that sshd won't start automatically,
+# the choice of passwording them has been removed from the user.
 
 ssh_server() {
     while true; do
@@ -59,13 +69,16 @@ ssh_server() {
                 sudo ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -q -N "" < /dev/null
                 ED25519_fingerprint="$(ssh-keygen -l -f /etc/ssh/ssh_host_ed25519_key.pub)"
                 RSA_fingerprint="$(ssh-keygen -l -f /etc/ssh/ssh_host_rsa_key.pub)"
-                sudo service sshd restart # Works on everything I tested but Wheezy with backported openssh-server. Included for maximum laziness
                 echo ""
                 echo "Your new host key fingerprints are:"
                 echo $ED25519_fingerprint
                 echo $RSA_fingerprint
                 echo ""
-                echo "Without closing this ssh session, add your public key to ~/.ssh/authorized_keys if it isn't there already, restart your sshd (If it wasn't restarted already), remove the line from your known_hosts file which corresponds to this server, and try logging in. If it works, HAPPY DANCE!"
+                echo "Without closing this ssh session, do the following:
+                1. Add your public key to ~/.ssh/authorized_keys if it isn't there already
+                2. Restart your sshd.
+                3. Remove the line from the ~/.ssh/known_hosts file on your computer which corresponds to this server.
+                Try logging in. If it works, HAPPY DANCE!"
                 break;;
             [Nn]* ) exit;;
         esac
