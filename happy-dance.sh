9c2e4fad208c5fd285735a566eafe79a11f60009
_NSAKEY <root [at] abigisp [dot] com>
2015-07-30 04:23:56 -0500

    Some mintor code cleanup. Also added support for printing both MD5 and SHA256 key fingerprints. Lastly, added a bunch of comments to help better explain the code.

--- happy-dance.sh
+++ happy-dance.sh
@@ -22,15 +22,13 @@
 # "brew tap homebrew/dupes"
 # "brew install openssh --with-brewed-openssl"
 # This will give you a working version of OpenSSH with OpenSSL. Testing without
-# OpenSSL failed miserably, so installing it isn't optional.
+# OpenSSL failed miserably, so installing it is required.
 # 3. Another Mac user note: The script drops "unset SSH_AUTH_SOCK" in your
 # .bash_profile. This is needed so that you can connect to remote hosts. Check the
 # comments below if you wish to know more.
 
 # TO DO:
-# 1. Eventually rework the configs to support new options in OpenSSH,
-# like FingerprintHash in 6.8 and ChaCha20-poly1350@openssh.com in 6.9.
-# This wil
+# 1. Windows 10 support?
 
 # This script automates everything laid out in stribika's Secure Secure Shell.
 # Source: https://stribika.github.io/2015/01/04/secure-secure-shell.html
@@ -40,8 +38,9 @@ UNAME=`uname`
 
 echo "This script will give you an ssh config for clients and servers that should force the NSA to work for a living.
 Use -h for help.
-For an explanation of everything, check out Secure Secure Shell:
+For an explanation of everything used in the configs, check out Secure Secure Shell:
 https://stribika.github.io/2015/01/04/secure-secure-shell.html
+Check out the README and the script's source if you want to see how the sausage is made.
 "
 
 # The ssh_client function takes the time to check for the existence of keys
@@ -55,6 +54,9 @@ ssh_client() {
         sudo cp etc/ssh/ssh_config /etc/ssh/ssh_config # Removed $PWD
     fi
 
+    # If you don't already have ssh keys, they will be generated for you.
+    # If you do have keys, they won't be deleted, because that would be rude.
+
     if [ ! -f $HOME/.ssh/id_ed25519 ]; then
         ssh-keygen -t ed25519 -o -a 100
     else
@@ -67,7 +69,9 @@ ssh_client() {
         echo "You already have an RSA key! If it's not at least 4096 bits, you should delete or move it and re-run this script!"
     fi
 
-    if [ $UNAME = "Darwin" ]; then 
+    # This rather hackish check for OS X is only done so that the user's .bash_profile can be modified to make outgoing ssh connections work.
+
+    if [ $UNAME = "Darwin" ]; then
         echo "unset SSH_AUTH_SOCK" >> ~/.bash_profile
         unset SSH_AUTH_SOCK
         echo "Since you use Mac OS X, you had to have a small modification to your .bash_profile in order to connect to remote hosts. Read here and follow the links to learn more: http:/serverfault.com/a/486048"
@@ -93,6 +97,10 @@ ssh_server() {
         case $yn in
             [Yy]* ) echo "Replacing your ssh server configuration file..."
 
+        # Some platforms (Such as OpenBSD and NetBSD) store the moduli in /etc/moduli,
+        # instead of /etc/ssh/moduli. I dislike nested ifs on principle, but this one
+        # isn't too terrible.
+
                 if [ ! -f /etc/ssh/moduli ]; then
                     if [ ! -f /etc/moduli ]; then
                         echo "Your OS doesn't have an /etc/ssh/moduli file, so we have to generate one. This might take a while."
@@ -110,34 +118,71 @@ ssh_server() {
                     sudo mv "${HOME}/moduli" /etc/ssh/moduli
                 fi
 
+                # Some platforms stuff the ssh config files under /usr/local, and this is also
+                # the case if you've built your own ssh binary. So instead of doing $UNAME checks,
+                # I just opted to check whether /usr/local/etc/ssh exists. I have yet to find a
+                # scenario in which one of these two dir paths aren't used, so there is no
+                # baked in error handling if /usr/local/etc/ssh and /etc/ssh don't exist.
+
+                # As for what the branches in the if do, they each copy over the hardened config,
+                # rm the host key files, generate new keys, then store those keys in variables
+                # for printing later. You should always verify host key fingerprints,
+                # and you are more likely to do it if this script makes it easy for you.
+                # The variables are set up so that if you're using OpenSSH 6.5-6-7, the script
+                # will print just the MD5 fingerprints. If you're using OpenSSH 6.8 and above,
+                # it will print both the MD5 and SHA256 fingerprints. This means you can
+                # easily verify the key fingerprints on your next login without having to
+                # worry about your OpenSSH version.
+
                 if [ -d /usr/local/etc/ssh ]; then
                     sudo cp etc/ssh/sshd_config /usr/local/etc/ssh/sshd_config
+                    cd /usr/local/etc/ssh
+                    sudo rm ssh_host_*key*
+                    sudo ssh-keygen -t ed25519 -f ssh_host_ed25519_key -q -N "" < /dev/null
+                    sudo ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -q -N "" < /dev/null
                     ED25519_fingerprint="$(ssh-keygen -l -f /usr/local/etc/ssh/ssh_host_ed25519_key.pub)"
                     RSA_fingerprint="$(ssh-keygen -l -f /usr/local/etc/ssh/ssh_host_rsa_key.pub)"
-                    cd /usr/local/etc/ssh
+                    ED25519_fingerprint_MD5="$(ssh-keygen -l -E md5 -f /usr/local/etc/ssh/ssh_host_ed25519_key.pub 2> /dev/null)"
+                    RSA_fingerprint_MD5="$(ssh-keygen -l -E md5 -f /usr/local/etc/ssh/ssh_host_rsa_key.pub 2> /dev/null)"
                 else
                     sudo cp etc/ssh/sshd_config /etc/ssh/sshd_config
+                    cd /etc/ssh
+                    sudo rm ssh_host_*key*
+                    sudo ssh-keygen -t ed25519 -f ssh_host_ed25519_key -q -N "" < /dev/null
+                    sudo ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -q -N "" < /dev/null
                     ED25519_fingerprint="$(ssh-keygen -l -f /etc/ssh/ssh_host_ed25519_key.pub)"
                     RSA_fingerprint="$(ssh-keygen -l -f /etc/ssh/ssh_host_rsa_key.pub)"
-                    cd /etc/ssh
+                    ED25519_fingerprint_MD5="$(ssh-keygen -l -E md5 -f /etc/ssh/ssh_host_ed25519_key.pub 2> /dev/null)"
+                    RSA_fingerprint_MD5="$(ssh-keygen -l -E md5 -f /etc/ssh/ssh_host_rsa_key.pub 2> /dev/null)"
                 fi
 
-                sudo rm ssh_host_*key*
-                sudo ssh-keygen -t ed25519 -f ssh_host_ed25519_key -q -N "" < /dev/null
-                sudo ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -q -N "" < /dev/null
+                # This next bit of code just prints the key fingerprints. if the *_MD5
+                # variables contain anything at all, they will print. Otherwise, that's
+                # 2 fewer lines printed in your terminal.
 
                 echo ""
                 echo "Your new host key fingerprints are:"
                 echo $ED25519_fingerprint
                 echo $RSA_fingerprint
+                if [ -n "$ED25519_fingerprint_MD5" ]; then
+                    echo $ED25519_fingerprint_MD5
+                fi
+
+                if [ -n "$RSA_fingerprint_MD5" ]; then
+                    echo $RSA_fingerprint_MD5
+                fi
+                echo "Don't forget to verify these!"
                 echo ""
+
+                # Just some final instructions. Nothing too fancy.
+
                 echo "Without closing this ssh session, do the following:
                 1. Add your public key to ~/.ssh/authorized_keys if it isn't there already
                 2. Restart your sshd.
                 3. Remove the line from the ~/.ssh/known_hosts file on your computer which corresponds to this server.
-                Try logging in. If it works, HAPPY DANCE!"
+                4. Try logging in. If it works, HAPPY DANCE!"
                 break;;
-            [Nn]* ) exit;;
+            [Nn]* ) exit;; # This is what happens if you select no.
         esac
     done
 }
